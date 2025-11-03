d31df13129e6942e43b1bbda461ef638e1edd371
ArnavSalad <122931650+ArnavSalad@users.noreply.github.com>
2025-11-03 17:21:47 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ["*.preview.same-app.com"],
+  images: {
+    unoptimized: true,
+    domains: [
+      "source.unsplash.com",
+      "images.unsplash.com",
+      "ext.same-assets.com",
+      "ugc.same-assets.com",
+      "contentgrid.homedepot-static.com",
+      "lovelygreens.com",
+      "hips.hearstapps.com",
+      "m.media-amazon.com",
+      "homesteadandchill.com",
+      "cdn11.bigcommerce.com",
+      "images.thdstatic.com",
+      "www.bhg.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ext.same-assets.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ugc.same-assets.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
